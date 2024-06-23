021fe529a4107c707bc1716fa38cad09f2adbcab
bforbilly24 <halimputra0701@gmail.com>
2024-06-24 01:26:52 +0700

    feat(page): add components/sections/Faq3

---
+++ components/sections/Faq3.js
@@ -0,0 +1,24 @@
+import Accordion1 from "../elements/Accordion1"
+
+
+export default function Faq3() {
+    return (
+        <>
+
+            <section className="tf-faq tf-section tf-faq-ss">
+                <div className="tf-container">
+                    <div className="row justify-content-center">
+                        <div className="col-md-12">
+                            <div className="title-ss wow fadeInUp">
+                                <h3>FAQS</h3>
+                            </div>
+                        </div>
+                        <div className="col-md-8">
+                            <Accordion1 />
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
+    )
+}
