92945c46807994bde5c7546b4723141028d16cb4
bforbilly24 <halimputra0701@gmail.com>
2024-06-24 01:26:59 +0700

    feat(page): add components/sections/Logo

---
+++ components/sections/Logo.js
@@ -0,0 +1,37 @@
+
+
+export default function Logo() {
+    return (
+        <>
+
+            <section className="logo-slider">
+                <div className="logo-slider-wrap">
+                    <div className="logo-slider-inner">
+                        <h3>BINABOX</h3>
+                        <h3>BINABOX</h3>
+                        <h3>BINABOX</h3>
+                        <h3>BINABOX</h3>
+                        <h3>BINABOX</h3>
+                        <h3>BINABOX</h3>
+                        <h3>BINABOX</h3>
+                        <h3>BINABOX</h3>
+                        <h3>BINABOX</h3>
+                        <h3>BINABOX</h3>
+                    </div>
+                    <div className="logo-slider-inner style-2">
+                        <h3>BINABOX</h3>
+                        <h3>BINABOX</h3>
+                        <h3>BINABOX</h3>
+                        <h3>BINABOX</h3>
+                        <h3>BINABOX</h3>
+                        <h3>BINABOX</h3>
+                        <h3>BINABOX</h3>
+                        <h3>BINABOX</h3>
+                        <h3>BINABOX</h3>
+                        <h3>BINABOX</h3>
+                    </div>
+                </div>
+            </section>
+        </>
+    )
+}
