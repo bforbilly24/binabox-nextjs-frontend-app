d354a25a385324f89c24ff397f6f17c4ba214745
bforbilly24 <halimputra0701@gmail.com>
2024-06-23 21:11:41 +0700

    feat(page): add components/elements/Accordion1

---
+++ components/elements/Accordion1.js
@@ -0,0 +1,36 @@
+'use client'
+import { useState } from 'react'
+export default function Accordion1() {
+    const [isActive, setIsActive] = useState(1)
+
+    const handleClick = (key) => {
+        setIsActive(prevState => prevState === key ? null : key)
+    }
+    return (
+        <>
+            <div className="tf-flat-accordion2">
+                <div className="flat-toggle2 active">
+                    <h6  className={isActive === 1 ? "toggle-title active" : "toggle-title"} onClick={() => handleClick(1)}>What are the NFTs?</h6>
+                    <div className="toggle-content" style={{ display: `${isActive === 1 ? "block" : "none"}` }}>
+                        <p>Urna vitae erat et lacus, consectetur ac nulla vestibulum lobortis. Nulla dapibus urna volutpat venenatis, risus faucibus.
+                        </p>
+                    </div>
+                </div>
+                <div className="flat-toggle2">
+                    <h6  className={isActive === 2 ? "toggle-title active" : "toggle-title"} onClick={() => handleClick(2)}>How do i get NFTs?</h6>
+                    <div className="toggle-content" style={{ display: `${isActive === 2 ? "block" : "none"}` }}>
+                        <p>Urna vitae erat et lacus, consectetur ac nulla vestibulum lobortis. Nulla dapibus urna volutpat venenatis, risus faucibus.
+                        </p>
+                    </div>
+                </div>
+                <div className="flat-toggle2">
+                    <h6  className={isActive === 3 ? "toggle-title active" : "toggle-title"} onClick={() => handleClick(3)}>How we can buy and invest NFT ?</h6>
+                    <div className="toggle-content" style={{ display: `${isActive === 3 ? "block" : "none"}` }}>
+                        <p>Urna vitae erat et lacus, consectetur ac nulla vestibulum lobortis. Nulla dapibus urna volutpat venenatis, risus faucibus.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
